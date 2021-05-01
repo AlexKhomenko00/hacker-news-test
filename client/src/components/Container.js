12cd574f73623dbe3688b3ed1e2b6cea9df3a198
Alex Khomenko <sasvsha@gmail.com>
2021-05-01 13:59:48 +0200

    enhanced container for mobile devices

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,5 +1,5 @@
 const Container = ({ children }) => (
-  <div className="container mx-80% mx-auto">{children}</div>
+  <div className="container w-11/12 mx-auto">{children}</div>
 );
 
 export default Container;
