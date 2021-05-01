4d812c46eb43da4f332bd296fa42fc3b874f0858
Alex Khomenko <sasvsha@gmail.com>
2021-05-01 12:46:00 +0200

    BASE_URL fix

--- client/src/api/news.js
+++ client/src/api/news.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:8006/api/news";
+const BASE_URL = "https://hacker-news-test-task.herokuapp.com/";
 
 export const fetchLatestNews = async () => {
   try {
